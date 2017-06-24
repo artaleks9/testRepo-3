11c09268c9707ffb46f0b208c2450fab546ad4f8
artaleks9 <userad9pf6@some.mail>
2017-06-24 14:02:02 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1498041117978
+//first_change1498310571016
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
