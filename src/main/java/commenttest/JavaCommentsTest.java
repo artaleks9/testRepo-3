622b37128d5944ea1b553071f73be2c8d538ac1f
artaleks9 <che@eclipse.org>
2017-10-10 10:09:04 +0000

    commit_in_first_cloned

--- src/main/java/commenttest/JavaCommentsTest.java
+++ src/main/java/commenttest/JavaCommentsTest.java
@@ -1,4 +1,4 @@
-//first_change1507299537799
+//first_change1507629877411
 /*
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
